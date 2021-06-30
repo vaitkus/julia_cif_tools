7d816d181313fada8122822d727cff3cceb5a48e
James.Hester <jxh@ansto.gov.au>
2021-06-30 13:14:44 +1000

    Add caseless compare, improve cmd line interface.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Command-line tools for working with Crystallographic Information Framework files
 # Installation
 
 1.  [Install Julia](https://docs.julialang.org/en/v1/manual/getting-started/)
-2.  At the Julia prompt, type `using Pkg;Pkg.add("CrystalInfoFramework")`. Exit Julia.
+2.  At the Julia prompt, type `using Pkg;Pkg.add("CrystalInfoFramework");Pkg.add("ArgParse")`. Exit Julia.
 3.  Copy or clone this project to a suitable location on your system. The instructions below assume that you are
     executing from this location.
 4.  A help message for each of the programs below can be obtained by running `julia <program_file>` at your command prompt.
@@ -25,13 +25,16 @@ repository](https://github.com/COMCIFS/cif_core).
 
 Check that two dictionaries have identical contents.
 
-Execute by running `julia compare.jl <lang> <ws> <dic1> <dic2>`. If `ws` is false, ignore whitespace differences.
-`lang` is the language (`ddl2` or `ddlm`) that the dictionaries are written in. Note that only a subset of DDL2
+Execute by running `julia compare.jl --lang <lang> -w -c <dic1>
+<dic2>`. If `-w`, ignore whitespace differences.  If `-c`, ignore case
+differences.  `lang` is the language (`ddl2` or `ddlm`) that the
+dictionaries are written in. Note that only a subset of DDL2
 attributes are currently checked.
 
 ## Pretty printer
 
 Format a dictionary according to layout style guide.
 
-Execute by running `julia pretty_print.jl <before> <after>` where `before` is the input dictionary file and `after` 
-is the name of the pretty-printed file.
+Execute by running `julia pretty_print.jl <before> <after>` where
+`before` is the input dictionary file and `after` is the name of the
+pretty-printed file.
--- compare.jl
+++ compare.jl
@@ -1,5 +1,5 @@
 # Compare two dictionaries, producing a report
-using CrystalInfoFramework,DataFrames
+using CrystalInfoFramework,DataFrames,ArgParse
 
 const ddl2_ref_dic = "ddl2_with_methods.dic"
 const ddlm_ref_dic = "/home/jrh/COMCIFS/cif_core/ddl.dic"
@@ -29,6 +29,13 @@ const ddl2_ignore = ()
 #    ((:item,:mandatory_code),(:category,:nx_mapping_details),
 #                     (:category,:mandatory_code))
 #
+const result_counter = Dict{Symbol,Int}(:defs=>0,:loops=>0,:probs=>0)
+
+print_err(s) = begin
+    result_counter[:probs] += 1
+    println(s)
+end
+
 # Find definitions that are present in one and not the other
 #
 find_missing_defs(dica,dicb) = begin
@@ -43,23 +50,22 @@ end
 # Find attributes in the definition in dica from cat that are missing or
 # different in dicb
 #
-report_missing_attrs(defa,defb,name,cat,ref_dic;ignore=[]) = begin
+check_one_def(defa,defb,name,cat,ref_dic;kwargs...) = begin
     if haskey(defa,cat)
         adef = defa[cat]
         if haskey(defb,cat)
             bdef = defb[cat]
-            compare_loops(adef,bdef,name,cat,ref_dic,ignore=ignore)
+            compare_loops(adef,bdef,name,cat,ref_dic;kwargs...)
+            result_counter[:loops] += 1
         else
-            println("$cat is missing from $name in second dictionary")
-            println("First dictionary has $adef")
-        end
-        
+            print_err("$cat is missing from $name in second dictionary\nFirst dictionary has $adef")
+        end 
     end
 end
 
-compare_loops(loop1,loop2,name,cat,ref_dic;ignore=[],wspace=false) = begin
+compare_loops(loop1,loop2,name,cat,ref_dic;ignore=[],wspace=false,caseless=false) = begin
     if nrow(loop1) != nrow(loop2)
-        println("$cat has different number of rows for $name")
+        print_err("$cat has different number of rows for $name")
     end
     if nrow(loop1) == 0 return end
     # check columns
@@ -68,7 +74,7 @@ compare_loops(loop1,loop2,name,cat,ref_dic;ignore=[],wspace=false) = begin
     bnames = names(loop2)
     do_not_have = setdiff(anames,bnames,ignorance,["master_id","__object_id","__blockname"])
     if length(do_not_have) > 0
-        println("$name: missing $do_not_have")
+        print_err("$name: missing $do_not_have")
     end
     common = setdiff(intersect(anames,bnames),ignorance,["master_id","__object_id","__blockname"])
     # println("$cat")
@@ -76,26 +82,28 @@ compare_loops(loop1,loop2,name,cat,ref_dic;ignore=[],wspace=false) = begin
     if nrow(loop1) > 1
         catkeys = get_keys_for_cat(ref_dic,"$cat")
         catobjs = Symbol.([find_object(ref_dic,x) for x in catkeys])
-        nonmatch = check_matching_rows(loop1,loop2,catobjs,wspace)
+        nonmatch = check_matching_rows(loop1,loop2,catobjs,wspace,caseless)
         if nrow(nonmatch) > 0
-            println("The following rows do not have matching keys for $cat:")
-            println("$nonmatch")
+            print_err("The following rows do not have matching keys for $cat in $name:\n$nonmatch")
         end
     end
     # now check all
-    nonmatch = check_matching_rows(loop1,loop2,common,wspace)
+    nonmatch = check_matching_rows(loop1,loop2,common,wspace,caseless)
     #println("Checked $common for $cat")
     if nrow(nonmatch) > 0
-        println("The following rows have at least one mismatched value for $cat:")
-        println("$nonmatch")
+        print_err("The following rows have at least one mismatched value for $cat in $name:\n$nonmatch")
     end
 end
 
-check_matching_rows(dfa,dfb,keylist,wspace) = begin
+check_matching_rows(dfa,dfb,keylist,wspace,caseless) = begin
     #println("Checking rows $keylist")
-    if !wspace
+    if wspace
         dfa = remove_wspace(dfa)
         dfb = remove_wspace(dfb)
+    end
+    if caseless
+        dfa = make_lower(dfa)
+        dfb = make_lower(dfb)
     end
     test = antijoin(dfa,dfb;on=keylist,validate=(false,true))
     return test
@@ -110,7 +118,16 @@ remove_wspace(df) = begin
     return df
 end
 
-report_diffs(source_lang,dics;wspace=false) = begin
+make_lower(df) = begin
+    for n in propertynames(df)
+        df[:,n] = map(df[!,n]) do x
+            typeof(x) <: AbstractString ? lowercase(x) : x
+        end
+    end
+    return df
+end
+
+report_diffs(source_lang,dics;kwargs...) = begin
     if source_lang == "ddl2"
         ref_dic = DDL2_Dictionary(ddl2_ref_dic)
         dica,dicb = DDL2_Dictionary.(dics)
@@ -126,18 +143,18 @@ report_diffs(source_lang,dics;wspace=false) = begin
     println("$test_categories")
     difa,_ = find_missing_defs(dica,dicb)
     if length(difa) > 0
-        println("Warning: missing definitions for $difa")
+        print_err("Warning: missing definitions for $difa")
     end
     for one_def in sort(collect(keys(dica)))
-        println("\n#=== $one_def ===#\n")
+        result_counter[:defs] += 1
         if one_def in difa
-            println("$one_def missing from second dictionary")
+            print_err("$one_def missing from second dictionary")
             continue
         end
         defa = dica[one_def]
         defb = dicb[one_def]
         for one_cat in test_categories
-            report_missing_attrs(defa,defb,one_def,one_cat,ref_dic,ignore=ignore)
+            check_one_def(defa,defb,one_def,one_cat,ref_dic;ignore=ignore,kwargs...)
         end
     end
     # Check top level
@@ -146,25 +163,47 @@ report_diffs(source_lang,dics;wspace=false) = begin
     println("\n=============\n\n Top level categories \n")
     for one_cat in keys(toplevela)
         if nrow(toplevela[one_cat]) == 0 continue end
-        println("\n#=== $one_cat ===#\n")
         if !(one_cat in keys(toplevelb)) || nrow(toplevelb[one_cat]) == 0
-            println("$one_cat is missing from second dictionary")
+            print_err("$one_cat is missing from second dictionary")
             continue
         end
-        compare_loops(toplevela[one_cat],toplevelb[one_cat],"toplevel",one_cat,ref_dic,ignore=ignore,wspace=wspace)
-    end      
+        compare_loops(toplevela[one_cat],toplevelb[one_cat],"toplevel",one_cat,ref_dic;ignore=ignore,kwargs...)
+    end
+    #
+    println("Summary\n=======\n")
+    println("Problems found: $(result_counter[:probs])")
+    println("Definitions checked: $(result_counter[:defs])")
+    println("Loops compared: $(result_counter[:loops])")
+end
+
+parse_cmdline() = begin
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+        "dictionary1"
+          help = "First dictionary to compare"
+          required = true
+        "dictionary2"
+          help = "Second dictionary to compare"
+          required = true
+        "-w"
+          help = "Ignore whitespace differences"
+          action = :store_true
+        "-c"
+          help = "Ignore case differences"
+          action = :store_true
+        "--lang"
+          arg_type = String
+          default = "ddlm"
+          help = "Dictionary language: 'ddlm' or 'ddl2'"
+    end
+    parse_args(s)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    if length(ARGS) < 4
-        println("""
-Usage: julia $(basename(PROGRAM_FILE)) <lang> <ws> <dictionary1> <dictionary2> 
-where <lang> is the language in which both dictionaries are written: either "ddl2" 
-or "ddlm". If <ws> is 'false', whitespace differences are ignored. """)
-        exit()
-    end
-    source_lang = ARGS[1]
-    wspace = parse(Bool,ARGS[2])
-    dics = (ARGS[3],ARGS[4])
-    report_diffs(source_lang,dics,wspace=wspace)
+    parsed_args = parse_cmdline()
+    source_lang = parsed_args["lang"]
+    wspace = parsed_args["w"]
+    caseless = parsed_args["c"]
+    dics = (parsed_args["dictionary1"],parsed_args["dictionary2"])
+    report_diffs(source_lang,dics,wspace=wspace,caseless=caseless)
 end
