b874501ee57995800963bf8f198dc28293b588b8
vaitkus <antanas.vaitkus90@gmail.com>
2021-09-23 18:01:56 +0300

    Remove some of the capitalisation rules as requested by the developers of the TOPOLOGY_CIF dictionary.

--- capitalisation.jl
+++ capitalisation.jl
@@ -16,8 +16,7 @@ const proper_names = ("Wyckoff","Cartn","_H_M\$","_H_M_","_Hall",
                       "I_over_I","I_over_netI","I_net","R_Fsqd","^R_I_","Lp_factor",
                       "R_I_factor","I_over_suI","meas_F","_S_",
                       "^R\$","^RT\$","^T\$","^B\$","^Ro\$","EPINET","_IZA\$",
-                      "RCSR","_SP\$","TOPOS\$","^TD10\$","Z_number","Voronoi",
-                      "^D_size\$","^Dsize\$"
+                      "RCSR","_SP\$","TOPOS\$","Voronoi"
                       )
 
 mutable struct CapitalCheck <: Visitor_Recursive
